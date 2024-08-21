ab205e998295ab9c8d5d4e3525f35134b554a073
goriiin <afk00.17@bk.ru>
2024-08-21 16:48:24 +0300

    service session

---
+++ tathockey/web/back/internal/service/session/session.go
@@ -0,0 +1,89 @@
+package session
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"tat_hockey_pack/internal/interfaces"
+	"tat_hockey_pack/internal/models"
+	"tat_hockey_pack/internal/repository"
+	"time"
+)
+
+const (
+	delay = time.Hour * 24 * 90
+)
+
+var (
+	invalidSessionErr = errors.New("invalid session")
+)
+
+type Service struct {
+	repo repository.Session
+	log  *slog.Logger
+}
+
+func NewService(sessionRepo repository.Session, log *slog.Logger) *Service {
+	return &Service{
+		repo: sessionRepo,
+		log:  log,
+	}
+}
+
+func (s *Service) Check(ctx context.Context, sessionFromCookie string) (*models.Session, error) {
+	ses, err := s.repo.GetBySessionID(ctx, sessionFromCookie)
+	if err != nil {
+		s.log.Debug("repo.GetBySessionID", "error", err)
+		return nil, err
+	}
+	if ses.CreatedAt.Add(delay).Before(time.Now()) {
+		s.log.Debug("repo.GetBySessionID", "error", err)
+		return nil, invalidSessionErr
+	}
+
+	return ses, nil
+}
+
+func (s *Service) Create(ctx context.Context, u interfaces.User) (string, error) {
+	session := models.Session{
+		ID:     RandStringRunes(16),
+		UserID: u.GetID(),
+	}
+
+	err := s.repo.Create(ctx, &session)
+	if err != nil {
+		return "", err
+	}
+	return session.ID, nil
+}
+
+func (s *Service) DestroyCurrent(ctx context.Context) error {
+	session, err := FromContext(ctx)
+	if err != nil {
+		s.log.Error("service.session.DestroyCurrent",
+			"func", "FromContext",
+			"error", err)
+		return err
+	}
+
+	err = s.repo.Destroy(ctx, session.ID)
+	if err != nil {
+		s.log.Debug("service.session.Destroy", "error", err)
+		return err
+	}
+	s.log.Debug("service.session.Destroy",
+		"session", session,
+		"destroy", "done")
+	return nil
+}
+
+func (s *Service) DestroyAll(ctx context.Context, user interfaces.User) error {
+	s.log.Debug("service.session.DestroyAll", "user", user)
+	err := s.repo.DestroyAll(ctx, user.GetID())
+	if err != nil {
+		s.log.Debug("service.session.DestroyAll", "error", err)
+		return err
+	}
+
+	return nil
+}
---
+++ tathockey/web/back/internal/service/session/utils.go
@@ -0,0 +1,31 @@
+package session
+
+import (
+	"context"
+	"errors"
+	"math/rand"
+	"tat_hockey_pack/internal/models"
+)
+
+var (
+	letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	ErrNoAuth   = errors.New("no session found")
+)
+
+const sessionKey int = 1
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func FromContext(ctx context.Context) (*models.Session, error) {
+	sess, ok := ctx.Value(sessionKey).(*models.Session)
+	if !ok {
+		return nil, ErrNoAuth
+	}
+	return sess, nil
+}
