f8ef0ba540935f4f7eb92d0c4e741b9af9fe9ee7
goriiin <afk00.17@bk.ru>
2024-08-21 19:31:50 +0300

    добавил проверку наличия пользователя по уникальным ключам

--- tathockey/web/back/internal/repository/user.go
+++ tathockey/web/back/internal/repository/user.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"log/slog"
 	"tat_hockey_pack/internal/models"
@@ -18,6 +19,9 @@ const (
 	saveUser      = "insert into users (id, email, nickname, password) values ($1, $2, $3, $4) returning id;"
 	getUserByID   = `select id, nickname, email, password from users where id = $1;`
 	getByNickname = `select id, nickname,email, password from users where nickname = $1;`
+	getByEmail    = `select id, nickname,email from users where email = $1;`
+
+	checkUserByEmailOrNick = `select id from users where email = $1 or nickname = $2;`
 )
 
 func NewUserRepository(db *pgxpool.Pool, log *slog.Logger) *User {
@@ -25,6 +29,15 @@ func NewUserRepository(db *pgxpool.Pool, log *slog.Logger) *User {
 		db:  db,
 		log: log,
 	}
+}
+
+func rowToUser(row pgx.Row) (*models.User, error) {
+	u := &models.User{}
+	err := row.Scan(&u.ID, &u.Nickname, &u.Email, &u.Password)
+	if err != nil {
+		return nil, err
+	}
+	return u, nil
 }
 
 func (u *User) Save(user *models.User) (uint64, error) {
@@ -39,26 +52,50 @@ func (u *User) Save(user *models.User) (uint64, error) {
 }
 
 func (u *User) GetByNickname(username string) (*models.User, error) {
-	var user models.User
-	err := u.db.QueryRow(context.Background(), getByNickname, username).
-		Scan(&user.ID, &user.Nickname, &user.Email, &user.Password)
+	row := u.db.QueryRow(context.Background(), getByNickname, username)
 
+	user, err := rowToUser(row)
 	if err != nil {
 		u.log.Error("User.GetByNickname", "err", err.Error())
 		return nil, err
 	}
 	u.log.Debug("User.GetByNickname", "user", user)
-	return &user, nil
+	return user, nil
+}
+
+func (u *User) GetByEmail(email string) (*models.User, error) {
+	row := u.db.QueryRow(context.Background(), getByEmail, email)
+	user, err := rowToUser(row)
+	if err != nil {
+		u.log.Error("User.GetByEmail", "err", err.Error())
+		return nil, err
+	}
+	u.log.Debug("User.GetByEmail", "user", user)
+	return user, nil
+}
+
+func (u *User) CheckUser(email, nickname string) (uint64, error) {
+	var id uint64
+	err := u.db.QueryRow(context.Background(), checkUserByEmailOrNick, email, nickname).Scan(&id)
+
+	if err != nil {
+		u.log.Error("User.GetByEmail", "err", err.Error())
+		return 0, err
+	}
+	u.log.Debug("User.GetByEmail", "user", id)
+
+	return id, nil
 }
 
 func (u *User) GetByID(id int64) (*models.User, error) {
-	var user models.User
-	err := u.db.QueryRow(context.Background(), getUserByID, id).
-		Scan(&user.ID, &user.Nickname, &user.Email, &user.Password)
+	row := u.db.QueryRow(context.Background(), getUserByID, id)
+
+	user, err := rowToUser(row)
+
 	if err != nil {
 		u.log.Error("User.GetByID", "err", err.Error())
 		return nil, err
 	}
 	u.log.Debug("User.GetByID", "user", user)
-	return &user, nil
+	return user, nil
 }
