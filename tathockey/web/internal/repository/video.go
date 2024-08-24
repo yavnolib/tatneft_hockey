639c2b5d7e827f287bb546d3008f6ee0664fc706
goriiin <afk00.17@bk.ru>
2024-08-25 02:25:45 +0300

    video

--- tathockey/web/internal/repository/video.go
+++ tathockey/web/internal/repository/video.go
@@ -15,10 +15,9 @@ type Video struct {
 }
 
 const (
-	saveVideo = "insert into video (creator_id, name, hash, created_at, description) values ($1, $2, $3, $4, $5) returning id;"
-	getByHash = `select id, creator_id, name, hash, created_at, description from video where hash = $1`
-	getByID   = `select id, creator_id, name, hash, created_at, description from video where id = $1`
-	getByName = `select id, creator_id, name, hash, created_at, description from video where name = $1`
+	saveVideo = "insert into videos (name) values ($1) returning id;"
+	getByID   = `select id, name from videos where id = $1`
+	getByName = `select id, name from videos where name = $1`
 )
 
 func NewVideoRepository(db *pgxpool.Pool, logger *slog.Logger) *Video {
@@ -32,7 +31,7 @@ func (v *Video) Save(vi *models.Video) (uint64, error) {
 	v.log.Debug("Video.Save", "--", "run")
 	var id uint64
 	err := v.db.QueryRow(context.Background(), saveVideo,
-		vi.CreatorID, vi.Name, vi.Hash, vi.CreatedAt, vi.Description).
+		vi.Name).
 		Scan(&id)
 	if err != nil {
 		v.log.Error("Video.Save", "error", err)
@@ -42,24 +41,11 @@ func (v *Video) Save(vi *models.Video) (uint64, error) {
 	return id, nil
 }
 
-func (v *Video) GetByHash(hash string) (*models.Video, error) {
-	v.log.Debug("Video.GetByHash", "hash", hash)
-	var vi models.Video
-	err := v.db.QueryRow(context.Background(), getByHash, hash).
-		Scan(&vi.ID, &vi.CreatorID, &vi.Name, &vi.Hash, &vi.CreatedAt, &vi.Description)
-	if err != nil {
-		v.log.Error("Video.GetByHash", "error", err.Error())
-		return nil, err
-	}
-	v.log.Debug("Video.GetByHash", "video", vi)
-	return &vi, nil
-}
-
 func (v *Video) GetByID(id uint64) (*models.Video, error) {
 	v.log.Debug("Video.GetByID", "hash", id)
 	var vi models.Video
 	err := v.db.QueryRow(context.Background(), getByID, id).
-		Scan(&vi.ID, &vi.CreatorID, &vi.Name, &vi.Hash, &vi.CreatedAt, &vi.Description)
+		Scan(&vi.ID, &vi.Name)
 	if err != nil {
 		v.log.Error("Video.GetByID", "error", err.Error())
 		return nil, err
@@ -72,7 +58,7 @@ func (v *Video) GetByName(name string) (*models.Video, error) {
 	v.log.Debug("Video.GetByName", "hash", name)
 	var vi models.Video
 	err := v.db.QueryRow(context.Background(), getByName, name).
-		Scan(&vi.ID, &vi.CreatorID, &vi.Name, &vi.Hash, &vi.CreatedAt, &vi.Description)
+		Scan(&vi.ID, &vi.Name)
 	if err != nil {
 		v.log.Error("Video.GetByName", "error", err.Error())
 		return nil, err
