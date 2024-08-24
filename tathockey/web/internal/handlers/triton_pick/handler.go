4a7919a76dd97855d27c8f2a0fc9b1bd95f56a27
goriiin <afk00.17@bk.ru>
2024-08-25 02:20:58 +0300

    grpc

--- tathockey/web/internal/handlers/triton_pick/handler.go
+++ tathockey/web/internal/handlers/triton_pick/handler.go
@@ -5,31 +5,28 @@ import (
 	"google.golang.org/grpc"
 	"time"
 
-	grpc_service "tat_hockey_pack/api/grpc-client"
+	grpcServ "tat_hockey_pack/api/grpc-client"
 )
 
-func ProcessWithTriton(videoName string) ([]float32, []int32, error) {
+func ProcessWithTriton(videoName string) ([]uint64, []int32, error) {
 	conn, err := grpc.Dial("localhost:8001", grpc.WithInsecure())
 	if err != nil {
 		return nil, nil, err
 	}
 	defer conn.Close()
 
-	client := grpc_service.NewGRPCInferenceServiceClient(conn)
+	client := grpcServ.NewGRPCInferenceServiceClient(conn)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	// Формирование запроса в Triton
-	request := &grpc_service.ModelInferRequest{
+	request := &grpcServ.ModelInferRequest{
 		ModelName: "resnet18",
-		Inputs: []*grpc_service.ModelInferRequest_InferInputTensor{
-			{
-				Name:     "video_name",
-				Datatype: "BYTES",
-				Shape:    []int64{1},
-				Contents: &grpc_service.InferTensorContents{
-					BytesContents: [][]byte{[]byte(videoName)},
+		Parameters: map[string]*grpcServ.InferParameter{
+			"video_name": {
+				ParameterChoice: &grpcServ.InferParameter_StringParam{
+					StringParam: videoName,
 				},
 			},
 		},
@@ -42,7 +39,7 @@ func ProcessWithTriton(videoName string) ([]float32, []int32, error) {
 	}
 
 	// Обработка ответа Triton
-	secondsTensor := response.Outputs[0].Contents.Fp32Contents
+	secondsTensor := response.Outputs[0].Contents.Uint64Contents
 	intsTensor := response.Outputs[1].Contents.IntContents
 
 	return secondsTensor, intsTensor, nil
